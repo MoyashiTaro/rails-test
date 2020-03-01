12a249b132eb227fa8077c70a7d93267cf7d0c52
天野彰吾 <1200asmac@amanoshougonoMacBook-Pro.local>
2020-03-01 19:29:40 +0900

    fixed 0301

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,23 @@
 class TweetsController < ApplicationController
     def index
-        @tweets=Tweet.all
+        @tweets=Tweet.all.page(params[:page]).per(5)
     end
+
+    def new
+        
+    end
+
+    def create
+        
+        Tweet.create(
+            name:params[:name],
+            image:params[:image],
+            text:params[:text],
+
+        )
+        
+
+        
+    end
+
 end
